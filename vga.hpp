0b461ba51d064b61017188078f7da534580e7cf9
CobaltXII <sidatt64@gmail.com>
2019-04-27 16:59:37 -0400

    Added all 16 VGA color combinations as 32-bit colors

--- vga.hpp
+++ vga.hpp
@@ -12,3 +12,23 @@ Uint32 make_vga_argb8888(unsigned char v,
 		   vga_tone[g] << 8 |
 		   vga_tone[a];
 }
+
+// All 16 VGA color combinations converted into 32-bit ARGB colors.
+Uint32 vga_argb8888[] = {
+	make_vga_argb8888(0, 0, 0),
+	make_vga_argb8888(0, 0, 2),
+	make_vga_argb8888(0, 2, 0),
+	make_vga_argb8888(0, 2, 2),
+	make_vga_argb8888(2, 0, 0),
+	make_vga_argb8888(2, 0, 2),
+	make_vga_argb8888(2, 1, 0),
+	make_vga_argb8888(2, 2, 2),
+	make_vga_argb8888(1, 1, 1),
+	make_vga_argb8888(1, 1, 3),
+	make_vga_argb8888(1, 3, 1),
+	make_vga_argb8888(1, 3, 3),
+	make_vga_argb8888(3, 1, 1),
+	make_vga_argb8888(3, 1, 3),
+	make_vga_argb8888(3, 3, 1),
+	make_vga_argb8888(3, 3, 3),
+};
