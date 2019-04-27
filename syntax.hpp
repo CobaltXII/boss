e94502f368a85d24aa0914e22176c98a5b57e0f2
CobaltXII <sidatt64@gmail.com>
2019-04-27 17:02:32 -0400

    Added C syntax highlighter importer

--- syntax.hpp
+++ syntax.hpp
@@ -11,3 +11,13 @@ std::string highlight_mode_string[] = {
 	"c",
 	"cpp"
 };
+
+// Import syntax highlighting rules for C.
+namespace HI_c {
+#include "syntax_c.hpp"
+}
+
+// All file extensions that use the hm_c rules.
+std::string ext_hm_c[] = {
+	".c"
+};
