a2664b045817aa4adc186ff9ae04b059eefa8df4
CobaltXII <sidatt64@gmail.com>
2019-04-27 16:55:38 -0400

    Added void barf

--- extras.hpp
+++ extras.hpp
@@ -1,2 +1,8 @@
 // Credits (can be seen in a hex editor).
 static const char* credits = "BOSS text editor by Adam Sidat (CobaltXII), April of 2019";
+
+// Barf a message and exit.
+void barf(std::string error) {
+	std::cout << error << std::endl;
+	exit(-1);
+}
