f0855857974d250b0bc79a57733e91397b2cee7a
CobaltXII <sidatt64@gmail.com>
2019-04-27 16:56:49 -0400

    Defined the public section of class video_interface

--- video.hpp
+++ video.hpp
@@ -5,4 +5,12 @@ private:
 	SDL_Window* sdl_window = NULL;
 	SDL_Renderer* sdl_renderer = NULL;
 	SDL_Texture* sdl_texture = NULL;
+	
+public:
+	// Dimensions of the SDL_Window*.
+	int x_res;
+	int y_res;
+	
+	// Video memory pointer.
+	Uint32* video = NULL;
 };
