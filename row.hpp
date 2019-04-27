937441de5ffdc5877c38512e183ccf8c38acf053
CobaltXII <sidatt64@gmail.com>
2019-04-27 17:00:20 -0400

    Added the default constructor of class row

--- row.hpp
+++ row.hpp
@@ -4,4 +4,14 @@ public:
 	// If this row contains an unclosed multiline comment (missing the
 	// two-character end sequence), the 'open' flag will be set.
 	bool open = false;
+
+	// Conversion from std::string to row.
+	row(std::string text = "",
+		unsigned char fg = vga_gray,
+		unsigned char bg = vga_black)
+	{
+		for (unsigned int i = 0; i < text.length(); i++) {
+			push_back({text[i], fg, bg});
+		}
+	}
 };
