a7151146a5b3a878432aef3d49d4bc6159e05030
CobaltXII <sidatt64@gmail.com>
2019-04-27 17:03:38 -0400

    Added all include directives

---
+++ boss.cpp
@@ -0,0 +1,30 @@
+/*
+ * #############################################################################
+ * #############################################################################
+ *
+ * BOSS text editor by Adam Sidat (CobaltXII), April of 2019
+ *
+ * #############################################################################
+ * #############################################################################
+ *
+ */
+
+#include <memory>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+#include <SDL.h>
+
+#include "extras.hpp"
+#include "mario.hpp"
+#include "glyph.hpp"
+#include "video.hpp"
+#include "font.hpp"
+#include "vga.hpp"
+#include "row.hpp"
+
+#include "syntax.hpp"
+#include "editor.hpp"
